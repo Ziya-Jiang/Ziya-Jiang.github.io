a1b786f9c8383df65355a0648127e01b5d9fe69a
Ziya-Jiang <jpf@stu.pku.edu.cn>
2025-12-02 02:58:46 +0000

    Deploying to gh-pages from @ Ziya-Jiang/Ziya-Jiang.github.io@bf47de04edbcc632e94226a5ca0fd3689a696097 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -495,16 +495,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-this-website-has-officially-started-updating-and-will-be-maintained-for-life",
+          title: 'This website has officially started updating and will be maintained for life.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
